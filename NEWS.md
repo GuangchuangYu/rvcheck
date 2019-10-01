38a758d998c40c920daef142484133c7aa5dfdad
Guangchuang Yu <guangchuangyu@gmail.com>
2019-10-01 14:53:55 +0800

    fixed #5

---
+++ NEWS.md
@@ -0,0 +1,33 @@
+# CHANGES IN VERSION 0.1.5
+
++ compatible with new major bump (R-4) (2019-10-01, Tue)
+  - currently not yet release and has empty folder in <https://cran.r-project.org/src/base/R-4/>
+
+# CHANGES IN VERSION 0.1.4
+ 
++ use default repository setting (2019-05-23, Thu)
+
+# CHANGES IN VERSION 0.1.3
+ 
++ change BiocInstaller to BiocManager
+
+# CHANGES IN VERSION 0.1.0
+ 
++ get_aes_var function
+
+# CHANGES IN VERSION 0.0.9
+ 
++ support checking gitlab version (2017-03-19, Sun)
+
+# CHANGES IN VERSION 0.0.8
+ 
++ add read.db,+, get_fun_from_pkg functions (2017-03-08, Wed)
+ 
+# CHANGES IN VERSION 0.0.7
+ 
++ fixed typo (2016-10-19, Wed)
+
+# CHANGES IN VERSION 0.0.5
+ 
++ update_github <2016-09-13, Tue)
+
--- R/check_r.R
+++ R/check_r.R
@@ -15,13 +15,23 @@ check_r <- function() {
     x <- readLines(base_url)
     r <- gsub(".*(R-\\d).*", '\\1', x[grep("R-\\d", x)])
 
-    r_major <- max(r)
-    r_major_url <- paste0(base_url, r_major)
-    y <- readLines(r_major_url)
-
-    pattern <- "R-\\d\\.\\d\\.\\d\\.tar\\.gz"
-    y <- y[grep(pattern, y)]
-    r_version <- gsub(paste0('.*(', pattern, ').*'), '\\1', y)
+    ## r_major <- max(r)
+    ## in case they bump major version and has empty folder in it.
+    ## e.g. as of 2019-10-01.
+    ## they create R-4, https://cran.r-project.org/src/base/R-4/
+    ## and there is nothing in it.
+    ## current release is R-3.6.1
+    r <- sort(r, decreasing = TRUE)
+    for(r_major in r) {
+        r_major_url <- paste0(base_url, r_major)
+        y <- readLines(r_major_url)
+        
+        pattern <- "R-\\d\\.\\d\\.\\d\\.tar\\.gz"
+        y <- y[grep(pattern, y)]
+        r_version <- gsub(paste0('.*(', pattern, ').*'), '\\1', y)
+        if (length(r_version))
+            break
+    }
 
     r_latest <- max(r_version)
     r_latest_version <- gsub("\\.tar\\.gz", "", r_latest)
