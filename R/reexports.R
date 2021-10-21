0e8d2dbacd9202b5212998eb484c0366fc2a7e3e
Andrew McDavid <andrew.n.mcdavid@gmail.com>
2021-10-21 17:13:30 -0400

    Reexport some functions from yulab.utils

---
+++ R/reexports.R
@@ -0,0 +1,16 @@
+#' @importFrom yulab.utils get_fun_from_pkg
+#' @export
+yulab.utils::get_fun_from_pkg
+
+#' @importFrom yulab.utils is.installed
+#' @export
+yulab.utils::is.installed
+
+#' @importFrom yulab.utils o
+#' @export
+yulab.utils::o
+
+#' @importFrom yulab.utils read.cb
+#' @export
+yulab.utils::read.cb
+
